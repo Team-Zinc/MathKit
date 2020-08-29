97d4b95e713cccad305dd44457bfae45317c4eb9
Eli Hatton <scrappye173@gmail.com>
2020-08-28 19:34:37 -0600

    began main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import Linear_Regression as Lr
+import Linear_Regression as lrgs
 import quadratic as quad
+
+prompt = """
+Hello and welcome to Zinc Software's MathKit. 
+Type any command or type \"Help\" to see possible commands.
+ """
+command = ""
+while command.lower() != "quit":
+    command = input("> ")
+    if command.lower == "linear regression":
+        passS
